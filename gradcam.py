764173564c5ce4d91de131e8f3bd808b2c487e04
Mikael17125 <xnias.ananto@gmail.com>
2024-04-14 20:07:34 +0900

    update code

--- gradcam.py
+++ gradcam.py
@@ -1,39 +1,20 @@
 import cv2
 import numpy as np
 
+
 class GradCam:
-    """
-    Gradient-weighted Class Activation Mapping (Grad-CAM) implementation for visualizing
-    important regions in an input image based on the activations of a specific target layer.
-
-    Args:
-        model (torch.nn.Module): The trained neural network model.
-        target (torch.nn.Module): The target layer in the model for which Grad-CAM will be computed.
-
-    Attributes:
-        model (torch.nn.Module): The trained neural network model.
-        feature (torch.Tensor): Feature maps extracted from the target layer.
-        gradient (torch.Tensor): Gradients of the target layer's output with respect to model's output.
-        target (torch.nn.Module): The target layer in the model for which Grad-CAM will be computed.
-    """
-
     def __init__(self, model, target):
         self.model = model.eval()
         self.feature = None
         self.gradient = None
+        self.handlers = []
         self.target = target
         self._get_hook()
 
     def _get_features_hook(self, module, input, output):
-        """
-        Hook function to extract feature maps from the target layer.
-        """
         self.feature = self.reshape_transform(output)
 
     def _get_grads_hook(self, module, input_grad, output_grad):
-        """
-        Hook function to compute gradients of the target layer's output with respect to model's output.
-        """
         self.gradient = self.reshape_transform(output_grad)
 
         def _store_grad(grad):  
@@ -42,16 +23,10 @@ class GradCam:
         output_grad.register_hook(_store_grad)
     
     def _get_hook(self):
-        """
-        Register hooks for extracting feature maps and computing gradients of the target layer.
-        """
         self.target.register_forward_hook(self._get_features_hook)
         self.target.register_forward_hook(self._get_grads_hook)
 
     def reshape_transform(self, tensor, height=14, width=14):
-        """
-        Reshape and transpose the tensor to facilitate Grad-CAM computation.
-        """
         result = tensor[:, 1:, :].reshape(tensor.size(0),
                                           height, 
                                           width, 
@@ -61,15 +36,7 @@ class GradCam:
         return result
 
     def __call__(self, inputs):
-        """
-        Compute Grad-CAM for the given input image.
-
-        Args:
-            inputs (torch.Tensor): The input image tensor.
-
-        Returns:
-            numpy.ndarray: The computed Grad-CAM heatmap.
-        """
+            
         self.model.zero_grad()
         output = self.model(inputs) 
         
@@ -81,11 +48,6 @@ class GradCam:
         weight = np.mean(gradient, axis=(1, 2))
         feature = self.feature[0].cpu().data.numpy()
 
-        # Theorem for Grad-CAM computation:
-        # Grad-CAM = Σ(∂L/∂A_k * A_k)
-        # Where:
-        # ∂L/∂A_k : Gradient of the target class activation with respect to the feature maps of the target layer.
-        # A_k : Feature maps of the target layer.
         cam = feature * weight[:, np.newaxis, np.newaxis]
         cam = np.sum(cam, axis=0) 
         cam = np.maximum(cam, 0) 
--- main.py
+++ main.py
@@ -1,61 +1,45 @@
-import cv2
-import numpy as np
 import timm
+import torch
 from skimage import io
-
+from torchvision.models import vgg19, resnet18
+from torchsummary import summary
 from gradcam import GradCam
+import numpy as np
+import cv2
+
+timm.list_models('vit_*')
 
 def prepare_input(image):
-    """
-    Preprocesses the input image to make it compatible with the model.
+    image = image.copy()
 
-    Args:
-        image (numpy.ndarray): The input image as a NumPy array.
-
-    Returns:
-        torch.Tensor: The preprocessed image as a PyTorch tensor.
-    """
     means = np.array([0.5, 0.5, 0.5])
     stds = np.array([0.5, 0.5, 0.5])
-    image = ((image.astype(np.float32) / 255) - means) / stds
-    image = np.transpose(image, (2, 0, 1))
-    return torch.tensor(image[np.newaxis, ...], requires_grad=True)
+    image -= means
+    image /= stds
+
+    image = np.ascontiguousarray(np.transpose(image, (2, 0, 1)))
+    image = image[np.newaxis, ...]
+
+    return torch.tensor(image, requires_grad=True)
 
 def gen_cam(image, mask):
-    """
-    Generates the Class Activation Map (CAM) using the input image and mask.
-
-    Args:
-        image (numpy.ndarray): The input image as a NumPy array.
-        mask (numpy.ndarray): The Grad-CAM mask as a NumPy array.
-
-    Returns:
-        numpy.ndarray: The Class Activation Map as a NumPy array.
-    """
     heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+
     cam = (1 - 0.5) * heatmap + 0.5 * image
-    return np.uint8(255 * cam / np.max(cam))
+    cam = cam / np.max(cam)
+    return np.uint8(255 * cam)
 
 if __name__ == '__main__':
-    # Load the input image
     img = io.imread("both.png")
     img = np.float32(cv2.resize(img, (224, 224))) / 255
-
-    # Prepare the input for the model
     inputs = prepare_input(img)
 
-    # Load the Vision Transformer model
     model = timm.create_model('vit_base_patch16_224', pretrained=True)
     target_layer = model.blocks[-1].norm1
     
-    # Initialize the Grad-CAM
     grad_cam = GradCam(model, target_layer)
-
-    # Compute the Grad-CAM mask
     mask = grad_cam(inputs)
-
-    # Generate the Class Activation Map
     result = gen_cam(img, mask)
 
-    # Save the resulting image
     cv2.imwrite('result.jpg', result)
